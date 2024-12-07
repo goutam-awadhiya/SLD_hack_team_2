cae1b5c1c13bcb3a553457756197e2195df5adf5
Aircounsel123 <poddarshreya001@gmail.com>
2024-12-07 12:03:14 +0530

    Update song_list.py

--- song_list.py
+++ song_list.py
@@ -1,22 +1,53 @@
-emotion_to_songs = {
+motion_to_songs = {
     "HAPPY": [
         {"title": "Happy - Pharrell Williams", "url": "https://www.youtube.com/watch?v=ZbZSe6N_BXs"},
-        {"title": "Can't Stop The Feeling - Justin Timberlake", "url": "https://www.youtube.com/watch?v=ru0K8uYEZWw"}
+        {"title": "Can't Stop The Feeling - Justin Timberlake", "url": "https://www.youtube.com/watch?v=ru0K8uYEZWw"},
+        {"title": "Uptown Funk - Mark Ronson ft. Bruno Mars", "url": "https://www.youtube.com/watch?v=OPf0Y6SoxE0"},
+        {"title": "Good as Hell - Lizzo", "url": "https://www.youtube.com/watch?v=1q0B3Gg2W7I"},
+        {"title": "Walking on Sunshine - Katrina and the Waves", "url": "https://www.youtube.com/watch?v=iPUmE-tne5u"}
     ],
     "SAD": [
         {"title": "Someone Like You - Adele", "url": "https://www.youtube.com/watch?v=hLQl3WQQoQ0"},
-        {"title": "Fix You - Coldplay", "url": "https://www.youtube.com/watch?v=k4V3Mo61fJM"}
+        {"title": "Fix You - Coldplay", "url": "https://www.youtube.com/watch?v=k4V3Mo61fJM"},
+        {"title": "All I Want - Kodaline", "url": "https://www.youtube.com/watch?v=8jXxI1l8W9A"},
+        {"title": "You Were Beautiful - DAY6", "url": "https://www.youtube.com/watch?v=8T9p1dX7P2I"},
+        {"title": "Tears Dry on Their Own - Amy Winehouse", "url": "https://www.youtube.com/watch?v=FZ9Zz8t4rYI"}
     ],
     "ANGRY": [
         {"title": "Rolling in the Deep - Adele", "url": "https://www.youtube.com/watch?v=rYEDA3JcQqw"},
-        {"title": "Stronger - Kanye West", "url": "https://www.youtube.com/watch?v=PsO6ZnUZI0g"}
+        {"title": "Stronger - Kanye West", "url": "https://www.youtube.com/watch?v=PsO6ZnUZI0g"},
+        {"title": "Killing in the Name - Rage Against the Machine", "url": "https://www.youtube.com/watch?v=5xFh2F4c5fM"},
+        {"title": "Break Stuff - Limp Bizkit", "url": "https://www.youtube.com/watch?v=4x1g0D8n2hU"},
+        {"title": "Kill This Love - BLACKPINK", "url": "https://www.youtube.com/watch?v=2S2SgV5H7eE"}
     ],
     "CALM": [
         {"title": "Weightless - Marconi Union", "url": "https://www.youtube.com/watch?v=UfcAVejslrU"},
-        {"title": "Clair de Lune - Debussy", "url": "https://www.youtube.com/watch?v=ea2WoUtbzuw"}
+        {"title": "Clair de Lune - Debussy", "url": "https://www.youtube.com/watch?v=ea2WoUtbzuw"},
+        {"title": "River Flows in You - Yiruma", "url": "https://www.youtube.com/watch?v=7ma1v5GqjXQ"},
+        {"title": "Sunset Lover - Petit Biscuit", "url": "https://www.youtube.com/watch?v=YcH9D6sY3sE"},
+        {"title": "Come Away With Me - Norah Jones", "url": "https://www.youtube.com/watch?v=tP1D7a9P8iE"}
     ],
     "SURPRISED": [
         {"title": "Shallow - Lady Gaga, Bradley Cooper", "url": "https://www.youtube.com/watch?v=bo_efYhYU2A"},
-        {"title": "Shake It Off - Taylor Swift", "url": "https://www.youtube.com/watch?v=nfWlot6h_JM"}
-    ]
+        {"title": "Shake It Off - Taylor Swift", "url": "https://www.youtube.com/watch?v=nfWlot6h_JM"},
+        {"title": “Rolling in the Deep - Adele”, “url”: “https://www.youtube.com/watch?v=rYEDA3JcQqw”},
+        {"title":"What Makes You Beautiful - One Direction","url":"https://www.youtube.com/watch?v=QJO3ROT-A4E"},
+        {"title":"Somebody That I Used To Know - Gotye","url":"https://www.youtube.com/watch?v=8UVNT4wvIGY"}
+    ],
+    # Additional moods
+    “EXCITED”: [
+        {“title”: “Can’t Stop - Red Hot Chili Peppers”, “url”: “https://www.youtube.com/watch?v=BfOdWSiykag”},
+    ],
+    “MELANCHOLIC”: [
+        {“title”: “The Night We Met - Lord Huron”, “url”: “https://www.youtube.com/watch?v=KtlgYxa6BMU”},
+    ],
+    “DISAPPOINTED”: [
+        {“title”: “Back to December - Taylor Swift”, “url”: “https://www.youtube.com/watch?v=QUwxK9GkVbA”},
+    ],
+    “ACTIVE”: [
+        {“title”: “Uptown Funk - Mark Ronson ft. Bruno Mars”, “url”: “https://www.youtube.com/watch?v=OPf0Y6SoxE0”},
+    ],
+    “INSPIRED”: [
+        {“title”: “Rise Up - Andra Day”, “url”: “https://www.youtube.com/watch?v=u9Dg-g7t2l4”},
+    ],
 }
